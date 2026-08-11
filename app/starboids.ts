f1fecc1a7cf9d99da205b1f659c28e0a9c63cc7e
jjbain <jjbain@bu.edu>
2026-08-10 21:59:08 -0400

    added moving boids (no behaviors)

--- app/starboids.ts
+++ app/starboids.ts
@@ -2,16 +2,57 @@ import * as THREE from "three";
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { Pane } from "tweakpane";
 
+interface boid {
+    size: number
+    color: THREE.Color
+    wireframe: boolean
+    position: THREE.Vector3
+    rotation: THREE.Vector3
+    velocity: THREE.Vector3
+    speed: number
+}
+
 export default function starboids(canvasRef: React.RefObject<HTMLCanvasElement | null>) {
     // initialize the scene
     const scene = new THREE.Scene();
 
-    // add objects to the scene
-    const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-    const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red", wireframe: true });
-    const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    // initialize objects
+    const boids: boid[] = [
+        {
+            size: .1,
+            color: new THREE.Color(0xffa800),
+            wireframe: false,
+            position: new THREE.Vector3(0, 0, 0),
+            rotation: new THREE.Vector3(THREE.MathUtils.degToRad(90), 0, 0),
+            velocity: new THREE.Vector3(Math.random(), Math.random(), 0),
+            speed: .01
+        }
+    ]
 
-    scene.add(cubeMesh);
+    for (var i = 0; i < 20; i++) {
+        boids.push({
+            size: .1,
+            color: new THREE.Color('grey'),
+            wireframe: true,
+            position: new THREE.Vector3(Math.random() * 2, Math.random() * 2, 0),
+            rotation: new THREE.Vector3(THREE.MathUtils.degToRad(90), 0, 0),
+            velocity: new THREE.Vector3(Math.random(), Math.random(), 0),
+            speed: (Math.random() * 2 - 1) * .01
+        })
+    }
+
+    // add boids to the scene
+    const allBoids = new THREE.Group();
+    const boidMeshes = boids.forEach((boid) => {
+
+        const boidGeometry = new THREE.CircleGeometry(boid.size, 0);
+        const boidMaterial = new THREE.MeshBasicMaterial({ color: boid.color, wireframe: boid.wireframe });
+        const boidMesh = new THREE.Mesh(boidGeometry, boidMaterial);
+        boidMesh.scale.x = 1.25
+        allBoids.add(boidMesh)
+    })
+
+    scene.add(allBoids);
 
     // initialize the camera
     const camera = new THREE.PerspectiveCamera(
@@ -33,7 +74,6 @@ export default function starboids(canvasRef: React.RefObject<HTMLCanvasElement |
     // instantiate the controls
     const controls = new OrbitControls(camera, canvasRef.current!);
     controls.enableDamping = true;
-    // controls.autoRotate = true;
 
     window.addEventListener("resize", () => {
         camera.aspect = window.innerWidth / window.innerHeight;
@@ -42,24 +82,31 @@ export default function starboids(canvasRef: React.RefObject<HTMLCanvasElement |
     });
 
     // initialize clock
-
     const clock = new THREE.Timer()
     let previousTime = clock.getElapsed();
 
     // render the scene
     const renderloop = () => {
-
         const currentTime = clock.getElapsed()
         const timeDelta = currentTime - previousTime
         console.log(timeDelta)
 
-        previousTime = currentTime
+        allBoids.children.forEach((boid, index) => {
+            const myBoidObject = boids[index]
+            boid.position.addScaledVector(myBoidObject.velocity, myBoidObject.speed)
+            boid.rotation.z = Math.atan2(myBoidObject.velocity.y * myBoidObject.speed, myBoidObject.velocity.x * myBoidObject.speed)
 
-        cubeMesh.rotation.y += THREE.MathUtils.degToRad(1) * timeDelta * 20
-        cubeMesh.rotation.z += THREE.MathUtils.degToRad(1) * timeDelta * 200
-        cubeMesh.scale.setScalar(Math.sin(currentTime * 2) * .25 + 1.25)
-        cubeMesh.rotateX(THREE.MathUtils.degToRad(Math.sin(currentTime * Math.random() * 2) * .25 + 1.25))
-
+            // bound the boids
+            if (boid.position.x > 2) {
+                boid.position.x = -2
+            }
+            if (boid.position.y > 2) {
+                boid.position.y = -2
+            }
+            if (boid.position.z > 2) {
+                boid.position.z = -2
+            }
+        })
 
         controls.update();
         renderer.render(scene, camera);
